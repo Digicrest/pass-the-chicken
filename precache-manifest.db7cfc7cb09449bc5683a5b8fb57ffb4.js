0cbe4c93b767cfcca5f138926aec2154170ee2c5
Izumi Koshiro <46288957+Digicrest@users.noreply.github.com>
2020-09-09 22:00:44 +0100

    Updates

---
+++ precache-manifest.db7cfc7cb09449bc5683a5b8fb57ffb4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "953277e956e687aa74394b2cb3b18d88",
+    "url": "/pass-the-chicken/index.html"
+  },
+  {
+    "revision": "213ecf4867f037f2c4cf",
+    "url": "/pass-the-chicken/static/css/main.a5f26f28.chunk.css"
+  },
+  {
+    "revision": "d81696b30dc0c246e2b1",
+    "url": "/pass-the-chicken/static/js/2.3423bfb8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/pass-the-chicken/static/js/2.3423bfb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "213ecf4867f037f2c4cf",
+    "url": "/pass-the-chicken/static/js/main.dcf701dd.chunk.js"
+  },
+  {
+    "revision": "e302e1127ec4753f0de6",
+    "url": "/pass-the-chicken/static/js/runtime-main.4e4d05f2.js"
+  }
+]);
